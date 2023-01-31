06a068a7a92e5dbb4e7f62881934c3e6710006d6
mtavidal <119874263+mtavidal@users.noreply.github.com>
2023-01-31 15:44:24 -0300

    Adiciona desafio site institucional - Gama

---
+++ script.js
@@ -0,0 +1,10 @@
+function clickMenu() {
+    if (listamenu.style.display == 'flex') {
+        listamenu.style.display = 'none'
+        menuburguer.style.color = '#2a7899'
+    } else {
+        listamenu.style.display = 'flex'
+        menuburguer.style.color = '#EAE0DA'
+    }
+    
+}
